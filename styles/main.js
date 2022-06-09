63c0d447ebe2df145fd6280f5461bad3f3a3ea82
Bonnie <55579065+bonnie-bg@users.noreply.github.com>
2022-06-09 13:49:06 +0000

    Add files via upload

---
+++ styles/main.js
@@ -0,0 +1,13 @@
+// show mobile menus
+
+$(document).ready(function() {
+    $("#icon").click(function() {
+        $("ul").toggleClass("show");
+    });
+});
+// let icon = document.getElementById("icon");
+// let img_back = document.querySelectorAll("img_back")
+
+// icon.addEventListener("click", function() {
+//     img_
+// })
